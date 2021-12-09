ab6db7218591bfd153c9331055cfc18975d1fdfc
Alin Ali Hassan <hassanalinali@gmail.com>
2021-12-09 13:02:14 +0100

    Added Terraform Cloud configuration

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,7 +2,7 @@ data "google_iam_policy" "viewer" {
   binding {
     role = "roles/storage.objectViewer"
     members = [
-        "allUsers",
-    ] 
+      "allUsers",
+    ]
   }
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,9 +1,17 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.1.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "4.3.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "alinalihassan"
+
+    workspaces {
+      name = "portfolio"
     }
   }
 }
