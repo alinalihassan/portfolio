e95bd54277aaa89785c15b69856d1289bc416711
Alin Ali Hassan <hassanalinali@gmail.com>
2023-01-06 15:15:06 +0100

    chore: updated dependencies

--- next.config.js
+++ next.config.js
@@ -1,40 +1,45 @@
 module.exports = {
-	poweredByHeader: false,
-	assetPrefix: process.env.ASSET_HOST || '',
-	productionBrowserSourceMaps: process.env.NODE_ENV === 'production',
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg?$/,
-			oneOf: [
-				{
-					use: [
-						{
-							loader: '@svgr/webpack',
-							options: {
-								prettier: false,
-								svgo: true,
-								svgoConfig: {
-									plugins: [{ removeViewBox: false }],
-								},
-								titleProp: true,
-							},
-						},
-					],
-					issuer: {
-						and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
-					},
-				},
-			],
-		});
+  poweredByHeader: false,
+  assetPrefix: process.env.ASSET_HOST || "",
+  productionBrowserSourceMaps: process.env.NODE_ENV === "production",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg?$/,
+      oneOf: [
+        {
+          use: [
+            {
+              loader: "@svgr/webpack",
+              options: {
+                prettier: false,
+                svgo: true,
+                svgoConfig: {
+                  plugins: [
+                    {
+                      name: "removeViewBox",
+                      active: false,
+                    },
+                  ],
+                },
+                titleProp: true,
+              },
+            },
+          ],
+          issuer: {
+            and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+          },
+        },
+      ],
+    });
 
-		return config;
-	},
-	publicRuntimeConfig: {},
-	images: {
-		domains: ['storage.googleapis.com'],
-	},
-	experimental: {
-		// Enables the styled-components SWC transform
-		styledComponents: true
-}
+    return config;
+  },
+  publicRuntimeConfig: {},
+  images: {
+    domains: ["storage.googleapis.com"],
+  },
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
 };
